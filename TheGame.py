49990938c900ebcfd2eca8bbd7215b699aed61b6
RichHarrison13 <60974796+RichHarrison13@users.noreply.github.com>
2020-02-12 15:40:16 +0000

    Initial_Commit

---
+++ TheGame.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-# 
+""""
+Created on Wed Feb 12 15:30:16 2020
+
+@author gv908497
+""""
+
+def main():
+    print("Hey World")
+    return 0
+
+if __name__ == "main__":
+    main()
